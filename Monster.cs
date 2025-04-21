c4db5acd39ccf94356992dd27cc23db4c0d5c15f
KyleK-UWW <KleinschK24@uww.edu>
2025-04-21 18:57:10 -0500

    Seperated Classes for Monster and Base Cards

---
+++ Monster.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace Final_Project
+{
+
+
+    public class Monster : Cards  //IAttack()?
+    {
+        int strength;
+        double hp;
+        public Monster(string Name, int Strength, double HP) : base(Name)
+        {
+            Strength = strength;
+        }
+        public int Strength
+        {
+            get => strength;
+            set
+            {
+                if (value <= 0 || value >= 100)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(strength));
+                }
+                strength = value;
+            }
+        }
+        public double HP
+        {
+            get => hp;
+            set
+            {
+                if (value >= 0 || value >= 999.99)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(hp));
+                }
+                hp = value;
+            }
+        }
+    }
+
+}
