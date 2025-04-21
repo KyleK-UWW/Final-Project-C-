6557bf04783ecc1c54e573b01b64574397d55df5
KyleK-UWW <KleinschK24@uww.edu>
2025-04-21 13:34:59 -0500

    Initial Push
    Basic card class and monster class

---
+++ Final Project.cs
@@ -0,0 +1,54 @@
+﻿public abstract class Cards
+{
+    string name;
+    public Cards(string Name)
+    {
+        Name = name;
+    }
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            name = value;
+        }
+    }
+}
+public class Monster : Cards  //IAttack()?
+{
+    int strength;
+    double hp;
+    public Monster(string Name,int Strength,double HP) : base(Name)
+    {
+        Strength = strength;
+    }
+    public int Strength
+    {
+        get=> strength;
+        set
+        {
+            if(value <= 0 ||  value >= 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(strength));
+            }
+            strength = value;
+        }
+    }
+    public double HP
+    {
+        get => hp;
+        set
+        {
+            if(value >= 0 || value >= 999.99)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hp));
+            }
+            hp = value;
+        }
+    }
+}
+
